6b6de64040669c47a7b3a106ed069537c3f11ad5
rinoshinme <rinoshinme@163.com>
2019-02-25 21:53:50 +0800

    Update kmp.h

--- algorithm/examples/kmp.h
+++ algorithm/examples/kmp.h
@@ -1,9 +1,27 @@
 #ifndef kmp_h
 #define kmp_h
 
+#include <vector>
+#include <string>
+
 namespace algo
 {
-    // kmp algorithm
+    // kmp algorithm, find pattern from input string
+    class KMP
+    {
+    private:
+        std::vector<int> next;
+    public:
+        // 56min
+        int bruteForce(const std::string& s, const std::string& p);
+        
+        int kmp();
+        
+    private:
+        void createNext(const std::string& pattern);
+        
+        
+    };
     
 }
 
